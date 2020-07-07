6892d913e642c0d0268ec3dc76e1ed2687b17f7b
Manohar <manohar.solleti@gmail.com>
2020-07-07 19:19:57 +0530

    👤 add signin and register pages

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
 import express from 'express';
 import data from './data';
+// import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoute';
+
+// dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((data) => {
+    console.log('mongodb connected');
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/users', userRoute);
 
 app.get('/api/products/:id', (req, res) => {
   const productId = req.params.id;
@@ -18,5 +40,5 @@ app.get('/api/products', (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log('Server started');
+  console.log('Server started at http://localhost:5000');
 });
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,14 +6,18 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducer';
+import { userSigninReducer, userRegisterReducer } from './reducers/userReducer';
 
 const cartItems = Cookie.getJSON('cartItems') || [];
+const userInfo = Cookie.getJSON('userInfo') || null;
 
-const initialState = { cart: { cartItems } };
+const initialState = { cart: { cartItems }, userSignin: { userInfo } };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
